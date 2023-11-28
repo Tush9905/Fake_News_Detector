79647a593d8a3dfe38e62ebe647ade3ac790cb4f
Tush9905 <tush9905@gmail.com>
2023-11-28 23:16:40 +0530

    v1.1
    Use proper Hugging Face Inference API and added more precise stats

--- App.py
+++ App.py
@@ -1,20 +1,33 @@
 import streamlit as st
 from transformers import pipeline
+import requests
+from huggingface_token import token
 
 st.write("# Fake News Detector")
-classifier = pipeline("text-classification", model="./fake_news_detector/")
+API_URL = "https://api-inference.huggingface.co/models/tush9905/email_fraud_detector"
+headers = {"Authorization": f"Bearer {token}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
 
 with st.form("my_form"):
     news_article = st.text_input(label="Copy and Paste the News Article Here.")
     submitted = st.form_submit_button("Submit")
     if submitted:
-        Result = classifier(news_article)
-        Label = Result[0]["label"]
-        Score = Result[0]["score"]
+        Result = query({"inputs": news_article})
+        Label = Result[0][0]["label"]
+        Score = Result[0][0]["score"]
+        Label_not = Result[0][1]["label"]
+        Score_not = Result[0][1]["score"]
         if Label == "RELIABLE":
             st.success(f"Looks {int(Score*100)}% {Label}")
         else:
             st.error(f"Looks {int(Score*100)}% {Label}")
+
+        st.write(f"{Label} : {Score*100}%")
+        st.write(f"{Label_not} : {Score_not*100}%")
+        
 
 
 
