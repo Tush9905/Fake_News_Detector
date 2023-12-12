fea0b4cae6146c5322fa9fcdcc8ab64e5f1ec63f
Tush9905 <tush9905@gmail.com>
2023-12-12 14:35:35 +0530

    Used proper .env file for huggingface token

--- App.py
+++ App.py
@@ -1,11 +1,14 @@
 import streamlit as st
-from huggingface_token import token
+import os
+from dotenv import load_dotenv
 import requests
 
 st.title("Fake News Detector")
 
+load_dotenv()
+huggingface_token = os.environ["HUGGINGFACE_HUB_TOKEN"]
 API_URL = "https://api-inference.huggingface.co/models/tush9905/fake_news_detector"
-headers = {"Authorization": f"Bearer {token}"}
+headers = {"Authorization": f"Bearer {huggingface_token}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
